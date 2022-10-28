1500f4bc8f4c7e2e008aba71059ca04ed8814919
Adriellison Ferreira <adriellison.ferreira@fitbank.com.br>
2022-10-28 13:27:09 -0300

    Variaveis e tipos, exemplos de convensão

---
+++ variaveisConstantes.py
@@ -0,0 +1,11 @@
+nome, idade = "Emanuel", 28
+
+nome = "Gionanna"
+
+print(nome, idade)
+
+limiteSaqueDiario = 1000.0
+print(limiteSaqueDiario)
+
+STATES = ["SP", "RJ", "CE", "SC", "RS"]
+print(STATES)
